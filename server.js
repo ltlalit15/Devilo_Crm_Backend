363103798502287da456df788c0b992e5a0d3a75
GautamBairagi <gauatambairagi22@gmail.com>
2025-12-24 11:51:20 +0530

    Mr Robot

--- server.js
+++ server.js
@@ -50,7 +50,7 @@ app.use(helmet());
 
 // CORS
 app.use(cors({
-  origin: process.env.FRONTEND_URL || 'http://localhost:5173',
+  origin: process.env.FRONTEND_URL || 'http://localhost:5173' || 'crmnew11.netlify.app',
   credentials: true
 }));
 
