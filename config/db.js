8fac20fdbdf12933b48e602a0feb5783ef1fd281
mahajanvishal123 <mahajanvishal9644@gmail.com>
2025-12-25 20:59:35 +0530

    first commit

--- config/db.js
+++ config/db.js
@@ -6,11 +6,11 @@ const mysql = require('mysql2/promise');
 
 // Create connection pool
 const pool = mysql.createPool({
-  host: process.env.DB_HOST || 'localhost',
-  user: process.env.DB_USER || 'root',
-  password: process.env.DB_PASS || '',
-  database: process.env.DB_NAME || 'crm_db',
-  port: parseInt(process.env.DB_PORT) || 3306,
+  host: process.env.DB_HOST ,
+  user: process.env.DB_USER ,
+  password: process.env.DB_PASS ,
+  database: process.env.DB_NAME,
+  port: parseInt(process.env.DB_PORT) ,
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0,
